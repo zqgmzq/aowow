380aff2fc593989e13a440e3cfaf0f00ee88b0f2
Helias <stefanoborzi32@gmail.com>
2023-01-06 23:37:10 +0100

    chore: update github link

--- template/pages/home.tpl.php
+++ template/pages/home.tpl.php
@@ -82,7 +82,7 @@ endif;
 
     <div class="footer">
         <div class="footer-links linklist">
-            <a href="?aboutus"><?=Lang::main('aboutUs'); ?></a>|<a href="https://github.com/Sarjuuk/aowow" target="_blank">Github</a>|<a href="#" id="footer-links-language"><?=Lang::main('language'); ?></a>
+            <a href="?aboutus"><?=Lang::main('aboutUs'); ?></a>|<a href="https://github.com/azerothcore/aowow" target="_blank">Github</a>|<a href="#" id="footer-links-language"><?=Lang::main('language'); ?></a>
         </div>
         <div class="footer-copy">
             &#12484; 2023 Aowow<br />
